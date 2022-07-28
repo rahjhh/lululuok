a1005208f3b90e5db51adcac7a208a08da32bd17
Genius <108347572+GeniusBoi@users.noreply.github.com>
2022-07-28 11:57:05 +0530

    Update vcbot.py

--- AdityaHalder/plugins/vcbot.py
+++ AdityaHalder/plugins/vcbot.py
@@ -58,7 +58,7 @@ async def ytdl_(link):
         return 0, stderr.decode()
 
 @app.on_message(command([".ply", "ply"]) & SUDOERS)
-@Client.on_message(command([".ply", "ply"]) & SUDOERS)
+#@Client.on_message(command([".ply", "ply"]) & SUDOERS)
 async def play(c: Client, m: Message):
     await m.delete()
     replied = m.reply_to_message
