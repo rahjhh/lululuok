e71acdaa28f023ee3d3d2990cce745b8c757d257
Genius <108347572+GeniusBoi@users.noreply.github.com>
2022-07-28 09:02:29 +0530

    Update clientbot.py

--- AdityaHalder/modules/clientbot/clientbot.py
+++ AdityaHalder/modules/clientbot/clientbot.py
@@ -6,5 +6,5 @@ from AdityaHalder.config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION
 client = Bot(STRING_SESSION, API_ID, API_HASH, plugins=dict(root="AdityaHalder.plugins"))
 robot = Bot(":memory:", API_ID, API_HASH, bot_token=BOT_TOKEN)
 
-pytgcalls = PyTgCalls(client)
+pytgcalls = PyTgCalls(client, overload_quiet_mode=False)
 
